792019337ff1232b580f8f9a7261dba6f1ef1b95
sunil parmar <sunil04ce@gmail.com>
2023-03-12 22:15:08 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+
+const HomePage = () => {
+  return (
+    <h1>Home page 2.</h1>
+  )
+}
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,7 @@
+const NewsPage = () => {
+    return (
+        <h1>News page.</h1>
+    )
+}
+
+export default NewsPage;
