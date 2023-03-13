01a04b6922a6a8dc2f1ce4fff75103ac943a6f9f
sunil parmar <sunil04ce@gmail.com>
2023-03-13 09:05:54 +0530

    navigated using Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
 const NewsPage = () => {
-    return (
-        <h1>News page.</h1>
-    )
-}
+  return (
+    <>
+      <h1>News page.</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great-framework">
+            NextJs is a great framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
+};
 
 export default NewsPage;
